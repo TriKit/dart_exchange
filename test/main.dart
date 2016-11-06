f5e1a798d474b3682d91a17f641c52062b9ec995
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2016-11-06 20:25:53 +0300

    Create blank test main.dart

--- test/main.dart
+++ test/main.dart
@@ -1,5 +1,10 @@
 import "package:test/test.dart";
+import '../lib/user.dart';
+import '../lib/asset.dart';
+import '../lib/offer.dart';
+import '../lib/contract.dart';
+import '../lib/balance.dart';
 
 void main() {
-  
+
 }
