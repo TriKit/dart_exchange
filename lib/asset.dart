e6b36e8555fd4929ddf493c7a1cf74e82d55c8f8
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2016-11-08 20:12:56 +0300

    Add static list assets, methods for adding new asset to assets and fix tests

--- lib/asset.dart
+++ lib/asset.dart
@@ -1,5 +1,24 @@
 class Asset {
   String code, description;
   Asset(this.code, this.description);
+  static var assets = [];
+
+  include(code) {
+    for (var a in assets) {
+      if (a.code == code) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  void pushNewAsset() {
+    if (!include(this.code)) {
+      assets.add(this);
+    }
+  }
+
   toString() => "$code $description";
+
 }
--- test/asset.dart
+++ test/asset.dart
@@ -3,9 +3,19 @@ import '../lib/asset.dart';
 
 
 void main() {
-  var asset = new Asset('usd', 'united states dollar');
+  var asset   = new Asset("usd", "united states dollar");
+  var asset_2 = new Asset("usd", "united states dollar");
+  var asset_3 = new Asset("euro", "eurozone currency");
 
-  test('toString() returns asset parameters in text form', () {
-    expect(asset.toString(), equals('usd united states dollar'));
+  asset.pushNewAsset();
+  asset_2.pushNewAsset();
+  asset_3.pushNewAsset();
+
+  test("toString() returns asset parameters in text form", () {
+    expect(asset.toString(), equals("usd united states dollar"));
+  });
+
+  test("pushNewAsset() function", () {
+    expect(Asset.assets.length, equals(2));
   });
 }
