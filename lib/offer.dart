3232226b3048ef146e824356768b86d2aa6d7c04
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2016-11-13 21:19:43 +0300

    Fix offer constructor

--- lib/offer.dart
+++ lib/offer.dart
@@ -1,18 +1,31 @@
+part of exchange;
+
 class Offer {
-  Asset ask;
-  Asset bid;
-  num amount;
-  num price;
+  Asset ask, bid;
+  num amount, price;
   Map belongs_to = {
     'user' : null
   };
 
+  Offer({ String ask: null, String bid: null, amount: null, price: null, user: null}) {
+     if (!user.balanceExists(bid) || user.getBalanceByCode(bid).amount <= amount) {
+       throw new Exception("Please check you balance");
+     }
+     else {
+       this.ask = Asset.find(ask);
+       this.bid = Asset.find(bid);
+       this.amount = amount;
+       this.price = price;
+       this.belongs_to["user"] = user;
+     }
+  }
+
   get user {
-    return belongs_to['user'];
+    return belongs_to["user"];
   }
 
   set user(u) {
-    setAssociation('user', u);
+    setAssociation("user", u);
     u.offers.add(this);
   }
 
