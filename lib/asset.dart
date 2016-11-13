f91f3daa51d66f0786636414c597138ed844496d
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2016-11-13 21:17:16 +0300

    Ad static method find

--- lib/asset.dart
+++ lib/asset.dart
@@ -1,6 +1,8 @@
+part of exchange;
+
 class Asset {
   String code, description;
-  
+
   Asset(String code, String description) {
     this.code = code;
     this.description = description;
@@ -17,6 +19,19 @@ class Asset {
         return false;
       }
     }
+  }
+
+  static Asset find(code) {
+    for(var a in Asset.assets) {
+      if(a.code == code)
+        return a;
+    }
+    // create new Asset
+    var a = new Asset(code, null);
+    // add that Asset to assets
+    Asset.assets.add(a);
+    // return that Asset
+    return a;
   }
 
   pushNewAsset() {
