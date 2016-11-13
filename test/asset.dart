650c38a6057f1d9037d6540c009394c52e327c37
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2016-11-13 21:17:53 +0300

    Add tests for find method

--- test/asset.dart
+++ test/asset.dart
@@ -1,6 +1,5 @@
 import "package:test/test.dart";
-import '../lib/asset.dart';
-
+import "../lib/exchange.dart";
 
 void main() {
   var asset   = new Asset("usd", "united states dollar");
@@ -13,5 +12,16 @@ void main() {
 
   test("pushNewAsset() function", () {
     expect(Asset.assets.length, equals(2));
+  });
+
+  group("find() method", () {
+    test("returns existance asset", () {
+      expect(Asset.find("usd"), equals(asset));
+    });
+
+    test("creates and returns new asset", () {
+      expect((Asset.find("rub") is Asset), isTrue);
+      expect(Asset.assets.last.code, equals("rub"));
+    });
   });
 }
