f95bad21e9764c61e15199cbaf6876f3f0523682
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2016-11-21 19:16:17 +0300

    Add association to user offers in constructor

--- lib/offer.dart
+++ lib/offer.dart
@@ -9,16 +9,17 @@ class Offer {
 
   // amount - количество продаваемого товара, price - цена за единицу товара
   Offer({ String ask: null, String bid: null, amount: null, price: null, user: null}) {
-    //  if (!user.balanceExists(bid) || user.getBalanceByCode(bid).amount < amount*price) {
-    //    throw new Exception("You currentrly have not enough balance");
-    //  }
-    //  else {
+     if (!user.balanceExists(bid) || user.getBalanceByCode(bid).amount < amount*price) {
+       throw new Exception("You currentrly have not enough balance");
+     }
+     else {
        this.ask = Asset.find(ask);
        this.bid = Asset.find(bid);
        this.amount = amount;
        this.price = price;
        this.belongs_to["user"] = user;
-    //  }
+       this.belongs_to["user"].addOffer(this);
+     }
   }
 
   get user {
@@ -34,5 +35,5 @@ class Offer {
     belongs_to[name] = object;
   }
 
-  toString() => "${this.belongs_to['user'].name} offers ${this.price} ${this.bid.code} for ${this.amount} ${this.ask.code}";
+  toString() => "${this.belongs_to['user'].name} offers ${this.price*this.amount} ${this.bid.code} for ${this.amount} ${this.ask.code}";
 }
