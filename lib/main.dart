8954bca5cd907f790a3095416a82d259c2801e0b
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2016-11-21 19:42:01 +0300

    Simplify main file

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,8 @@ main() {
   var buyer  = new User("John", "john@gmail.com", "secret");
 
   var seller_balance = new Balance(asset: "rub", amount: 10000, user: seller);
-  print(seller.associations["balances"]);
-  print(seller_balance.user.name);
-  var buyer_balance = new Balance(asset: "usd", amount: 100, user: buyer);
-  print(buyer.associations["balances"]);
-  print(buyer_balance.user.name);
-  var offer = new Offer(ask: "usd", bid: "rub", amount: 40, price: 65, user: seller);
-  print(offer);
-
-  var contract = new Contract(offer: offer, buyer: buyer);
+  var buyer_balance  = new Balance(asset: "usd", amount: 100, user: buyer);
+  var offer          = new Offer(ask: "usd", bid: "rub", amount: 30, price: 65, user: seller);
+  var contract       = new Contract(offer: offer, buyer: buyer);
   // contract.updateBalances();
 }
