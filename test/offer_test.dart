1dbb0eda4e0b297f0555a10284786ebb5c427cfc
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2016-11-21 23:20:31 +0300

    Fix offer test setUp

--- test/offer_test.dart
+++ test/offer_test.dart
@@ -5,19 +5,15 @@ void main() {
   var user, offer, balance, asset;
   setUp(() {
     user = new User("Ivan", "ivan@gmail.com", "secret");
-    asset = new Asset("usd", "unated states dollar");
-    balance = new Balance();
-    balance.asset = asset;
-    balance.amount = 100;
-    balance.user = user;
-    offer = new Offer(bid: "usd", ask: "rub", amount: 50, price: 3200, user: user);
+    balance = new Balance(asset: "rub", amount: 6000, user: user);
+    offer = new Offer(ask: "usd", bid: "rub", amount: 50, price: 65, user: user);
   });
 
   test("it creates new Offer", () {
-    expect(offer.ask.code, equals("rub"));
-    expect(offer.bid.code, equals("usd"));
+    expect(offer.ask.code, equals("usd"));
+    expect(offer.bid.code, equals("rub"));
     expect(offer.amount, equals(50));
-    expect(offer.price, equals(3200));
+    expect(offer.price, equals(65));
     expect(offer.user.name, equals("Ivan"));
   });
 }
