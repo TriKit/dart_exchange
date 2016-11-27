def9f3aaebc9386385824b1edfb3176a255a9ff6
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2016-11-27 23:14:36 +0300

    Add test to model noSuchMetod

--- test/model_test.dart
+++ test/model_test.dart
@@ -2,5 +2,19 @@ import "package:test/test.dart";
 import "../lib/exchange.dart";
 
 void main() {
-  
+  group("noSuchMethod", () {
+    test("it returns existent property", () {
+      var model = new Model();
+      expect(model.name, equals("SomeName"));
+    });
+
+    test("it throws error if property does't exists", () {
+      expect(() => new Model().hello, throws);
+    });
+
+    test("it sets value of existent property", () {
+      var model = new Model();
+      expect(model.name = "NewName", equals("NewName"));
+    });
+  });
 }
